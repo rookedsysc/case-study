12a11e3def56958f5d8f2c912f408d06f57194c0
rookedsysc <rookedsysc36@gmail.com>
2026-03-04 19:51:54 +0900

    init

---
+++ src/main/kotlin/com/roky/casestudy/CaseStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.roky.casestudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaseStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CaseStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/roky/casestudy/CaseStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roky.casestudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaseStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

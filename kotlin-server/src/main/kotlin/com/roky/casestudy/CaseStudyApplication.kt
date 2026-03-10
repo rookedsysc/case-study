603b84e7995ea0bd0eb735c03ea238a922c1953e
rookedsysc <rookedsysc36@gmail.com>
2026-03-10 19:59:24 +0900

    fix(infra): 애플리케이션 로그 수집 경로를 복구하고 exporter 설정을 수정
    OpenTelemetry SDK 초기화와 OTLP 전송 설정을 맞춰 Loki로 로그가 수집되도록 복구한다.
    mysqld-exporter 인증 전달 방식을 v0.16 권장 방식으로 변경해 기동 오류를 제거한다.

--- kotlin-server/src/main/kotlin/com/roky/casestudy/CaseStudyApplication.kt
+++ kotlin-server/src/main/kotlin/com/roky/casestudy/CaseStudyApplication.kt
@@ -1,5 +1,7 @@
 package com.roky.casestudy
 
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +10,10 @@ class CaseStudyApplication
 
 fun main(args: Array<String>) {
     runApplication<CaseStudyApplication>(*args)
+
+    val openTelemetrySdk = AutoConfiguredOpenTelemetrySdk
+        .builder()
+        .build()
+        .openTelemetrySdk
+    OpenTelemetryAppender.install(openTelemetrySdk)
 }
